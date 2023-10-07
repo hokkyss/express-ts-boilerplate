3bbd9dcc62c14d7a415712ddc597f697a60c063d
Hokki Suwanda <hokkyss2@gmail.com>
2023-10-07 12:08:44 +0700

    feat: add logger

--- constants/error.constant.ts
+++ constants/error.constant.ts
@@ -1,6 +1,8 @@
 enum ErrorCodes {
   InternalServerError = 'Internal Server Error',
+  NotFound = 'Not Found!',
   Unauthorized = 'Unauthorized!',
+  // TODO: add more codes, e.g. "user/not-found", "tags/bad-request", etc
 }
 
 export default ErrorCodes
--- server.ts
+++ server.ts
@@ -3,20 +3,17 @@ import type { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import express from 'express'
 import createHttpError from 'http-errors'
-import morgan from 'morgan'
+import { StatusCodes } from 'http-status-codes'
 
 import envConfig from './config/env.config'
 import ErrorCodes from './constants/error.constant'
+import authRoute from './routes/auth.route'
+import loggerService from './services/logger.service'
 
 const app = express()
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
-app.use(
-  morgan(
-    '[:date[clf]] - :status ":method :url HTTP/:http-version" :response-time ms'
-  )
-)
 app.use((_req, res, next) => {
   res
     .header('Access-Control-Allow-Headers', 'Origin, Content-type, Accept')
@@ -25,11 +22,27 @@ app.use((_req, res, next) => {
 })
 app.set('json spaces', 2)
 
+app.use((req, _res, next) => {
+  loggerService.info(req.path)
+  return next()
+})
+
+app.use('api/v1/auth', authRoute)
+
+app.use('*', (_req, res) => {
+  return res
+    .status(StatusCodes.NOT_FOUND)
+    .json({ details: ErrorCodes.NotFound })
+})
+
 app.use((err: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  loggerService.error(err)
   if (createHttpError.isHttpError(err)) {
     return res.status(err.statusCode).json({ details: err.message })
   }
-  return res.status(500).json({ details: ErrorCodes.InternalServerError })
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ details: ErrorCodes.InternalServerError })
 })
 
 app.listen(envConfig.port)
